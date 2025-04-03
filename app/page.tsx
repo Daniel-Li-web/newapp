6877c9c63c2a392fab92127a5d7a96d122496f5d
Daniel <lzhaolong059@gmail.com>
2025-04-03 10:30:13 +0700

    change

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function MyApp() {
-  return (
-    <div>Hello World!!</div>
+  return (<div>
+    <h1>Hello World!!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate voluptas cum suscipit numquam, accusamus, itaque facilis similique nobis, impedit voluptates exercitationem quos harum ipsa ullam ducimus. Similique, harum. Delectus, eveniet?</p>
+    </div>
   )
 }
